40faaab820449b6a2d78f3d21c5fb0b1a2dbdc40
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2025-01-01 20:37:01 +0530

    small change...

--- tutorial/index.js
+++ tutorial/index.js
@@ -2,12 +2,12 @@ import { ApolloServer } from "apollo-server"
 import resolvers from "./schema/resolvers.js"
 import typeDefs from "./schema/type-defs.js"
 
-const Port=7000;
+
 
 const server= new ApolloServer({typeDefs, resolvers});
 
 
 
-server.listen(Port,()=>{
-    console.log(`YOUR API IS RUNNING AT: ${Port} :)`);
-})
+server.listen().then(({ url }) => {
+    console.log(`YOUR API IS RUNNING AT: ${url} :)`);
+  });
